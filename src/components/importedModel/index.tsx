f28889b252afd5a6def72f972eb4abaa59f00ffe
IWSR <lsh934960645@163.com>
2023-11-17 04:45:13 +0800

    feat(model): 加入光源

--- src/components/importedModel/index.tsx
+++ src/components/importedModel/index.tsx
@@ -1,8 +1,9 @@
-// import { useEffect } from 'react';
+import { useRef } from "react";
 import { useLoader } from "@react-three/fiber";
 import { MMDLoader } from "three/addons/loaders/MMDLoader.js";
 import { OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
+import { useShadowCameraHelper } from "@/tools";
 
 function Model() {
   const result = useLoader(
@@ -19,11 +20,107 @@ function Model() {
   console.log(result, "result");
 
   return (
-    <primitive object={result} position={[0, 0, 0]} displacementScale={0.1} />
+    <primitive object={result} position={[0, 0, 0]} scale={[0.1, 0.1, 0.1]} />
   );
 }
 
 function ImportedModels() {
+  const dcLight = useRef<THREE.DirectionalLight>(null);
+  const spotLight = useRef<THREE.SpotLight>(null);
+  const pointLight = useRef<THREE.PointLight>(null);
+
+  const {
+    showDirectionalLight,
+    directionalLightIntensity,
+    directionalLightX,
+    directionalLightY,
+    directionalLightZ,
+  } = useControls({
+    showDirectionalLight: false,
+    directionalLightIntensity: {
+      value: 1,
+      min: 0,
+      max: 1,
+      step: 0.1,
+    },
+    directionalLightX: {
+      value: 1.23,
+      min: -5,
+      max: 5,
+      step: 0.001,
+    },
+    directionalLightY: {
+      value: 0.66,
+      min: -5,
+      max: 5,
+      step: 0.001,
+    },
+    directionalLightZ: {
+      value: 0,
+      min: -5,
+      max: 5,
+      step: 0.001,
+    },
+  });
+
+  const {
+    showSpotLight,
+    spotLightDistance,
+    spotLightX,
+    spotLightY,
+    spotLightZ,
+  } = useControls({
+    showSpotLight: false,
+    spotLightDistance: {
+      value: 10,
+      min: 0,
+      max: 10,
+      step: 0.001,
+    },
+    spotLightX: {
+      value: 0,
+      min: -5,
+      max: 5,
+      step: 0.001,
+    },
+    spotLightY: {
+      value: 2,
+      min: -5,
+      max: 5,
+      step: 0.001,
+    },
+    spotLightZ: {
+      value: 2,
+      min: -5,
+      max: 5,
+      step: 0.001,
+    },
+  });
+
+  const { showPointLight, pointLightX, pointLightY, pointLightZ } = useControls(
+    {
+      showPointLight: false,
+      pointLightX: {
+        value: -1,
+        min: -5,
+        max: 5,
+        step: 0.001,
+      },
+      pointLightY: {
+        value: 1,
+        min: -5,
+        max: 5,
+        step: 0.001,
+      },
+      pointLightZ: {
+        value: 0,
+        min: -5,
+        max: 5,
+        step: 0.001,
+      },
+    },
+  );
+
   const { ambientLightIntensity } = useControls({
     ambientLightIntensity: {
       value: 1,
@@ -33,10 +130,50 @@ function ImportedModels() {
     },
   });
 
+  useShadowCameraHelper(dcLight, showDirectionalLight);
+
+  useShadowCameraHelper(spotLight, showSpotLight);
+
+  useShadowCameraHelper(pointLight, showPointLight);
+
   return (
     <>
-      <ambientLight intensity={ambientLightIntensity} color={"pink"} />
+      <ambientLight intensity={ambientLightIntensity} color={0xffffff} />
       <Model />
+
+      {showDirectionalLight && (
+        <directionalLight
+          ref={dcLight}
+          intensity={directionalLightIntensity}
+          position={[directionalLightX, directionalLightY, directionalLightZ]}
+          color={0xffffff}
+          castShadow={showDirectionalLight}
+        />
+      )}
+
+      {showSpotLight && (
+        <spotLight
+          ref={spotLight}
+          intensity={1}
+          color={0xffffff}
+          distance={spotLightDistance}
+          decay={1}
+          angle={Math.PI * 0.3}
+          penumbra={1}
+          position={[spotLightX, spotLightY, spotLightZ]}
+          castShadow={showSpotLight}
+        />
+      )}
+
+      {showPointLight && (
+        <pointLight
+          ref={pointLight}
+          color={0xffffff}
+          intensity={1}
+          position={[pointLightX, pointLightY, pointLightZ]}
+          castShadow={showPointLight}
+        />
+      )}
       <OrbitControls />
     </>
   );
