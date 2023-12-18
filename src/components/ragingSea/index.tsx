1e8f3b93b91f69a2291523ad21546f4f1703a112
IWSR <lsh934960645@163.com>
2023-12-19 01:26:43 +0800

    feat(shaderdemo): ragingSea

--- src/components/ragingSea/index.tsx
+++ src/components/ragingSea/index.tsx
@@ -1,19 +1,69 @@
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
-import { useRef } from "react";
+import { useRef, useMemo } from "react";
+import { useFrame } from "@react-three/fiber";
 import vertexShader from "./shader/water/water.vert?raw";
 import fragmentShader from "./shader/water/water.frag?raw";
-console.log(vertexShader, fragmentShader);
+import { useControls } from "leva";
+
 function RagingSea() {
   const planeRef = useRef<THREE.Mesh | null>(null);
+  const shaderMaterialRef = useRef<THREE.ShaderMaterial | null>(null);
+
+  const { uBigWavesElevation, uBigWavesFrequencyX, uBigWavesFrequencyY } =
+    useControls({
+      uBigWavesElevation: {
+        value: 0.2,
+        min: 0,
+        max: 1,
+        step: 0.001,
+      },
+      uBigWavesFrequencyX: {
+        value: 4,
+        min: 0,
+        max: 10,
+        step: 0.001,
+      },
+      uBigWavesFrequencyY: {
+        value: 1.5,
+        min: 0,
+        max: 10,
+        step: 0.001,
+      },
+    });
+
+  const uniforms = useMemo(() => {
+    return {
+      uBigWavesElevation: { value: uBigWavesElevation },
+      uBigWavesFrequency: {
+        value: new THREE.Vector2(uBigWavesFrequencyX, uBigWavesFrequencyY),
+      },
+      uTime: { value: 0 },
+      uBigWavesSpeed: { value: 0.75 },
+    };
+  }, [uBigWavesElevation, uBigWavesFrequencyX, uBigWavesFrequencyY]);
+
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime();
+
+    if (shaderMaterialRef.current) {
+      shaderMaterialRef.current.uniforms.uTime.value = time;
+      shaderMaterialRef.current.uniforms.uBigWavesElevation.value =
+        uBigWavesElevation;
+      shaderMaterialRef.current.uniforms.uBigWavesFrequency.value =
+        new THREE.Vector2(uBigWavesFrequencyX, uBigWavesFrequencyY);
+    }
+  });
 
   return (
     <>
       <mesh ref={planeRef}>
         <planeGeometry args={[2, 2, 128, 128]} />
         <shaderMaterial
+          ref={shaderMaterialRef}
           fragmentShader={fragmentShader}
           vertexShader={vertexShader}
+          uniforms={uniforms}
         />
       </mesh>
       <OrbitControls />
--- src/components/ragingSea/shader/water/water.vert
+++ src/components/ragingSea/shader/water/water.vert
@@ -1,6 +1,14 @@
+uniform float uBigWavesElevation;
+uniform vec2 uBigWavesFrequency;
+uniform float uTime;
+uniform float uBigWavesSpeed;
+
 void main() {
-  // modelMatrix viewMatrix 在 shaderMaterial 内有声明
-  vec4 modelPosition = modelMatrix * vec4(position, 10);
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) * sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) * uBigWavesElevation;
+  modelPosition.y += elevation;
+
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
